441767593af43bf0bd82710f8bdaf713b2d823e6
cademccu <cade21mccumber@gmail.com>
2021-08-15 17:32:28 -0600

    clear functionality

--- src/mp3.py
+++ src/mp3.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 import youtube_dl
 import sys
+import subprocess
 
 
 
@@ -21,9 +22,22 @@ def download(URL):
         ydl.download([URL])
 
 
+def help():
+    print("[ USAGE ] \n")
+    print("<URL>                     Download the video or playlist at the youtube URL to mp3.")
+    print("--file | -f   <FILE>      Provide a text file with a youtube URL on every line.")
+    print("--help | -h               This help menu.")
+    print("--clear | -c              Clears youtube-dl's cache, can help if a download fails.")
+    sys.exit(0)
+
 
 def main():
-    if sys.argv[1] == "--file" or sys.argv[1] == "-f":
+    # TODO better arg management either in loop or dictionary
+    # need to check for argv length before indexing.
+    if len(sys.argv) == 1:
+        help()
+        sys.exit(0)
+    elif sys.argv[1] == "--file" or sys.argv[1] == "-f":
         with open(sys.argv[2], "r") as f:
             line = f.readline()
             while True:
@@ -32,10 +46,10 @@ def main():
                 download(line)
                 line = f.readline()
     elif sys.argv[1] == "--help" or sys.argv[1] == "-h":
-        print("[ USAGE ] \n")
-        print("--file | -f   <ARG>       Provide a text file with a youtube URL on every line.")
-        print("<ARG.                     Download the video or playlist at the youtube URL to mp3.")
-        print("--help | -h               This menu")
+        help()
+        
+    elif sys.argv[1] == "--clear" or sys.argv[1] == "-c":
+        subprocess.run(["youtube-dl", "--rm-cache-dir"])
     else: # single URL
         download(sys.argv[1])
 
