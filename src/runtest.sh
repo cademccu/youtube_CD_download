7dd05057e01f5a11381748829568dd996d5aaef8
cademccu <cade21mccumber@gmail.com>
2024-02-19 00:30:39 -0700

    Halfway done refactoring. Added option for format

---
+++ src/runtest.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#yt-dlp --extract-audio --audio-format best https://www.youtube.com/watch?v=u1v-TN6j0Rk
+
+
+python3 mp3 -u https://www.youtube.com/watch?v=u1v-TN6j0Rk -a mp8
+
+echo "test completed"
+echo "removing test mp3"
+
+rm "Charlotte de Witte - Overdrive (Original Mix) [KNTXT021].mp3"
+rm "Charlotte de Witte - Overdrive (Original Mix) [KNTXT021].opus"
+
+# wtf apple
+rm "mp3~"
+rm "runtest.sh~"
+
+echo "[DONE]"
+
