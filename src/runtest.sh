e059d59e900e9c56821d45a1975b88b4b1502c70
cademccu <cade21mccumber@gmail.com>
2024-02-19 19:13:28 -0700

    Final refactoring to get old standard working with upgradedc changes around quality and code maintainability

--- src/runtest.sh
+++ src/runtest.sh
@@ -3,7 +3,7 @@
 #yt-dlp --extract-audio --audio-format best https://www.youtube.com/watch?v=u1v-TN6j0Rk
 
 
-python3 mp3 -u https://www.youtube.com/watch?v=u1v-TN6j0Rk -a mp8
+python3 mp3 -u https://www.youtube.com/watch?v=u1v-TN6j0Rk
 
 echo "test completed"
 echo "removing test mp3"
